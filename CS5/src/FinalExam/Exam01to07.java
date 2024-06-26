f81e12348c770d5f074005dea30cb1f2a1dd9646
alanahn <alan.ahn@daddyslab.com>
2024-06-26 09:55:31 +0900

    240626_window_작업

---
+++ CS5/src/FinalExam/Exam01to07.java
@@ -0,0 +1,113 @@
+package ThirdExam_반_이름;
+
+/**
+ * [평가3]
+ * 시간 : 1시간 10분(70분)
+ * 문제 : 16문제(객관식 7문제 + 주관식 9문제)
+ *
+ * @author 이름(반) ex. 김아무개(토0900)
+ * */
+
+public class Exam01to07 {
+    public static void main(String[] args){
+        Exam00();
+        Exam01();
+        Exam02();
+        Exam03();
+        Exam04();
+        Exam05();
+        Exam06();
+        Exam07();
+    }
+
+    /*
+    [예시 문제]
+    문제0. 다음 중 듣고 있는 수업의 언어를 고르세요.
+    1) C
+    2) Java
+    3) Python
+    4) Kotlin
+    */
+    private static void Exam00(){
+        //" "사이에 정답이라고 생각되는 번호(ex."2")를 입력하세요.
+        //답이 두 개일 경우 "2,3"으로 입력하세요.
+        System.out.println("[문제0]: "+"2");
+    }
+
+
+    /*
+    문제1. 다음 중 자바의 특징으로 틀린 것을 고르세요.
+     1) 객체 중심 개발이다.
+     2) 가상 머신(JVM)을 활용해 플랫폼에 영향을 받지 않아 다양한 환경에서 사용 가능하다.
+     3) 절차 지향 언어이다.
+     4) 보안이 강하기 때문에 프로그램이 안정적이다.
+    */
+    private static void Exam01(){
+        System.out.println("[문제1]: "+"");
+    }
+
+
+    /*
+    문제2. 다음 중 패키지에 대한 설명으로 올바른 것을 고르세요.
+     1) 클래스 안에 만들어진 메소드이다.
+     2) 프로그램 전체에 접근 가능한 접근 제어자이다.
+     3) 표준 입출력 클래스이다.
+     4) 서로 비슷한 성격의 자바 클래스들을 모아 놓은 폴더이다.
+    */
+    private static void Exam02(){
+        System.out.println("[문제2]: "+"");
+    }
+
+    /*
+    문제3. 다음 중 인덱스의 범위에 대한 설명으로 올바른 것을 고르세요.
+     1) 0부터 배열의 길이
+     2) 1부터 배열의 길이
+     3) 1부터 배열의 길이-1
+     4) 0부터 배열의 길이-1
+    */
+    private static void Exam03(){
+        System.out.println("[문제3]: "+"");
+    }
+
+    /*
+    문제4. 다음 중 배열을 선언하거나 초기화한 것 중 틀린 것을 고르세요.
+     1) int arr[3];
+     2) int[] arr = {1,2,3,4};
+     3) int[] arr = new int[5]{1,2,3,4,5};
+     4) int[]arr = new int[5];
+    */
+    private static void Exam04(){
+        System.out.println("[문제4]: "+"");
+    }
+
+    /*
+    문제5. 다음 중 접근 제어자에 관한 설명으로 올바른 것을 고르세요.
+     1) private – 같은 패키지 내에서만 접근 가능
+     2) default – 같은 클래스 내에서만 접근 가능
+     3) protected – 같은 패키지 내에서, 그리고 다른 패키지의 자손 클래스에서 접근 가능
+     4) public – 서로 다른 패키지 내에서만 접근 가능
+
+    */
+    private static void Exam05(){
+        System.out.println("[문제5]: "+"");
+    }
+
+    /*
+    문제6. return문은 값을 반환하는 역할만 한다.
+     1) O
+     2) X
+    */
+    private static void Exam06(){
+        System.out.println("[문제6]: "+"");
+    }
+
+    /*
+    문제7. 클래스는 객체의 필드와 메소드로 구성되어 있다.
+     1) O
+     2) X
+    */
+    private static void Exam07(){
+        System.out.println("[문제7]: "+"");
+    }
+
+}
